a3409c9f7d4e02b92b61592205347fd63377c510
ErnTrgt <erntrgt10@gmail.com>
2024-03-07 01:37:57 +0300

    Update Model.js

--- helloworldthreejs/src/components/Scene/Model.js
+++ helloworldthreejs/src/components/Scene/Model.js
@@ -1,34 +1,51 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import { MeshTransmissionMaterial, useGLTF, Text } from "@react-three/drei";
-import { useFrame, useThree } from '@react-three/fiber'
-import { useControls } from 'leva'
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 
 export default function Model() {
     const { nodes } = useGLTF("/medias/torrus.glb");
-    const { viewport } = useThree()
+    const { viewport } = useThree();
     const torus = useRef(null);
-    
-    useFrame( () => {
-        torus.current.rotation.x += 0.02
-    })
+
+    useFrame(() => {
+        torus.current.rotation.x += 0.02;
+    });
 
     const materialProps = useControls({
         thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
         roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-        transmission: {value: 1, min: 0, max: 1, step: 0.1},
+        transmission: { value: 1, min: 0, max: 1, step: 0.1 },
         ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-        chromaticAberration: { value: 0.02, min: 0, max: 1},
-        backside: { value: true},
-    })
-    
+        chromaticAberration: { value: 0.02, min: 0, max: 1 },
+        backside: { value: true },
+    });
+
     return (
-        <group scale={viewport.width / 3.75} >
-            <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -1]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
-                hello world!
+        <group scale={viewport.width / 3.75}>
+            <Text
+                font={"/fonts/PPNeueMontreal-Bold.otf"}
+                position={[0, 0.5, -1]}
+                fontSize={0.5}
+                color='white'
+                anchorX='center'
+                anchorY='middle'
+            >
+                HELLO WORLD
+            </Text>
+            <Text
+                font={"/fonts/PPNeueMontreal-Bold.otf"}
+                position={[0, -0.5, -1]}
+                fontSize={0.65}
+                color='white'
+                anchorX='center'
+                anchorY='middle'
+            >
+                ERN TRGT{" "}
             </Text>
             <mesh ref={torus} {...nodes.Torus002}>
-                <MeshTransmissionMaterial {...materialProps}/>
+                <MeshTransmissionMaterial {...materialProps} />
             </mesh>
         </group>
-    )
+    );
 }
